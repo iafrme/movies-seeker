e15301e41b2ebcdcd76040174d78914870031845
aleks <aleksgnu@gmail.com>
2019-01-13 22:13:37 -0600

    Connect to api ⚛️

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Title } from './Components/Title';
+import { SearchForm } from './Components/SearchForm';
+
 import './App.css';
+import 'bulma/css/bulma.css';
 
 class App extends Component {
+  state = { results: [] };
+
+  _handleResults = results => {
+    this.setState({ results });
+  };
+
+  _renderResults() {
+    const { results } = this.state;
+    return results.map(movie => {
+      return <p key={movie.imdbID}>{movie.Title}</p>;
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Title>Buscador de Películas...</Title>
+        <div className="SearchForm-wrapper">
+          <SearchForm onResults={this._handleResults} />
+        </div>
+        {this.state.results.length === 0 ? (
+          <p>Sin resultados </p>
+        ) : (
+          this._renderResults()
+        )}
       </div>
     );
   }
